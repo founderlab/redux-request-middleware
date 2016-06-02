0f70966f1d4360dbbd4424d38e811c68472fbb45
gwilym <gwilym@gwilymh.com>
2016-06-02 11:21:15 +1000

    Added requestLogger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import createRequestMiddleware from './request'
+import createRequestLoggerMiddleware from './requestLogger'
 import createResponseParserMiddleware from './responseParser'
 
-export {createRequestMiddleware, createResponseParserMiddleware}
+export {createRequestMiddleware, createResponseParserMiddleware, createRequestLoggerMiddleware}
 export const requestMiddleware = createRequestMiddleware()
 export const responseParserMiddleware = createResponseParserMiddleware()
+export const requestLoggerMiddleware = createRequestLoggerMiddleware()
---
+++ src/requestLogger.js
@@ -0,0 +1,22 @@
+import _ from 'lodash'
+
+const defaults = {
+  getRequest: action => action.request,
+  logRequest: request => console.log('[request]', request.method, request.url, request),
+  getResponse: action => action.res,
+  logResponse: response => console.log('[response]', response),
+}
+
+export default function createRequestLoggerMiddleware(_options={}) {
+  const options = _.merge(defaults, _options)
+
+  return function requestLoggerMiddleware() {
+    return next => action => {
+      const request = options.getRequest(action)
+      if (request) options.logRequest(request)
+      const response = options.getResponse(action)
+      if (response) options.logResponse(response)
+      next(action)
+    }
+  }
+}
